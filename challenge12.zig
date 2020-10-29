531db75e7c92d5c0bb3de8628d8e69d75c639cb0
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-10-29 11:39:40 -0700

    Add isECB check to ch12

--- challenge12.zig
+++ challenge12.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const ecb = @import("./challenge7.zig");
 const pad = @import("./challenge9.zig");
+const StringHashMap = std.StringHashMap;
 const crypto = std.crypto;
 const mem = std.mem;
 const testing = std.testing;
@@ -8,9 +9,9 @@ const testing = std.testing;
 pub fn EncryptionOracle(comptime keysize: usize) type {
     return struct {
         const Self = @This();
-        const secret_suffix = [_]u8{0x52, 0x6f, 0x6c, 0x6c, 0x69, 0x6e, 0x27, 0x20, 0x69, 0x6e, 0x20, 0x6d, 0x79, 0x20, 0x35, 0x2e, 0x30, 0x0a, 0x57, 0x69, 0x74, 0x68, 0x20, 0x6d, 0x79, 0x20, 0x72, 0x61, 0x67, 0x2d, 0x74, 0x6f, 0x70, 0x20, 0x64, 0x6f, 0x77, 0x6e, 0x20, 0x73, 0x6f, 0x20, 0x6d, 0x79, 0x20, 0x68, 0x61, 0x69, 0x72, 0x20, 0x63, 0x61, 0x6e, 0x20, 0x62, 0x6c, 0x6f, 0x77, 0x0a, 0x54, 0x68, 0x65, 0x20, 0x67, 0x69, 0x72, 0x6c, 0x69, 0x65, 0x73, 0x20, 0x6f, 0x6e, 0x20, 0x73, 0x74, 0x61, 0x6e, 0x64, 0x62, 0x79, 0x20, 0x77, 0x61, 0x76, 0x69, 0x6e, 0x67, 0x20, 0x6a, 0x75, 0x73, 0x74, 0x20, 0x74, 0x6f, 0x20, 0x73, 0x61, 0x79, 0x20, 0x68, 0x69, 0x0a, 0x44, 0x69, 0x64, 0x20, 0x79, 0x6f, 0x75, 0x20, 0x73, 0x74, 0x6f, 0x70, 0x3f, 0x20, 0x4e, 0x6f, 0x2c, 0x20, 0x49, 0x20, 0x6a, 0x75, 0x73, 0x74, 0x20, 0x64, 0x72, 0x6f, 0x76, 0x65, 0x20, 0x62, 0x79, 0x0a};
+        const secret_suffix = [_]u8{ 0x52, 0x6f, 0x6c, 0x6c, 0x69, 0x6e, 0x27, 0x20, 0x69, 0x6e, 0x20, 0x6d, 0x79, 0x20, 0x35, 0x2e, 0x30, 0x0a, 0x57, 0x69, 0x74, 0x68, 0x20, 0x6d, 0x79, 0x20, 0x72, 0x61, 0x67, 0x2d, 0x74, 0x6f, 0x70, 0x20, 0x64, 0x6f, 0x77, 0x6e, 0x20, 0x73, 0x6f, 0x20, 0x6d, 0x79, 0x20, 0x68, 0x61, 0x69, 0x72, 0x20, 0x63, 0x61, 0x6e, 0x20, 0x62, 0x6c, 0x6f, 0x77, 0x0a, 0x54, 0x68, 0x65, 0x20, 0x67, 0x69, 0x72, 0x6c, 0x69, 0x65, 0x73, 0x20, 0x6f, 0x6e, 0x20, 0x73, 0x74, 0x61, 0x6e, 0x64, 0x62, 0x79, 0x20, 0x77, 0x61, 0x76, 0x69, 0x6e, 0x67, 0x20, 0x6a, 0x75, 0x73, 0x74, 0x20, 0x74, 0x6f, 0x20, 0x73, 0x61, 0x79, 0x20, 0x68, 0x69, 0x0a, 0x44, 0x69, 0x64, 0x20, 0x79, 0x6f, 0x75, 0x20, 0x73, 0x74, 0x6f, 0x70, 0x3f, 0x20, 0x4e, 0x6f, 0x2c, 0x20, 0x49, 0x20, 0x6a, 0x75, 0x73, 0x74, 0x20, 0x64, 0x72, 0x6f, 0x76, 0x65, 0x20, 0x62, 0x79, 0x0a };
 
-        var key: [keysize/8]u8 = undefined;
+        var key: [keysize / 8]u8 = undefined;
 
         allocator: *mem.Allocator,
 
@@ -28,8 +29,7 @@ pub fn EncryptionOracle(comptime keysize: usize) type {
             };
         }
 
-        pub fn deinit(self: *Self) void {
-        }
+        pub fn deinit(self: *Self) void {}
 
         pub fn encrypt(self: *const Self, plaintext: []const u8) ![]u8 {
             var secret_and_plaintext = try self.createInput(plaintext);
@@ -43,7 +43,7 @@ pub fn EncryptionOracle(comptime keysize: usize) type {
         }
 
         fn createInput(self: *const Self, plaintext: []const u8) ![]u8 {
-            const padded_len = pad.calcWithPkcsSize(keysize/8, secret_suffix.len + plaintext.len);
+            const padded_len = pad.calcWithPkcsSize(keysize / 8, secret_suffix.len + plaintext.len);
             var secret_and_plaintext = try self.allocator.alloc(u8, padded_len);
 
             // create (plaintext || suffix)
@@ -56,7 +56,7 @@ pub fn EncryptionOracle(comptime keysize: usize) type {
                 secret_and_plaintext[idx] = secret_suffix[idx];
             }
 
-            pad.pkcsPad(keysize/8, secret_and_plaintext, secret_and_plaintext[0..secret_suffix.len + plaintext.len]);
+            pad.pkcsPad(keysize / 8, secret_and_plaintext, secret_and_plaintext[0 .. secret_suffix.len + plaintext.len]);
             return secret_and_plaintext;
         }
     };
@@ -67,8 +67,8 @@ pub fn discoverBlockSize(allocator: *mem.Allocator, oracle: EncryptionOracle(128
     // 1. begin with input of size 1 (smallest) and make note of size
     // 2. grow input until size changes; make note of new size
     // 3. block size == size(2) - size(1)
-   
-    // limiting max blocksize to 256 
+
+    // limiting max blocksize to 256
     var payload = try allocator.alloc(u8, 256);
     defer allocator.free(payload);
 
@@ -76,7 +76,6 @@ pub fn discoverBlockSize(allocator: *mem.Allocator, oracle: EncryptionOracle(128
     for (payload) |*byte| {
         byte.* = 'A';
     }
-
 
     // get initial size of encryption
     var initial = try oracle.encrypt(payload[0..1]);
@@ -95,9 +94,37 @@ pub fn discoverBlockSize(allocator: *mem.Allocator, oracle: EncryptionOracle(128
     return error.BlockSizeTooLarge;
 }
 
+pub fn isOracleECB(allocator: *mem.Allocator, blocksize: u32, oracle: EncryptionOracle(128)) !bool {
+    var payload = try allocator.alloc(u8, 3 * blocksize);
+    defer allocator.free(payload);
+
+    for (payload) |*byte| {
+        byte.* = 'A';
+    }
+
+    var enc = try oracle.encrypt(payload[0..]);
+    defer allocator.free(enc);
+
+    // look for dupe blocks
+    var seen_blocks = StringHashMap(void).init(allocator);
+    defer seen_blocks.deinit();
+
+    var blk_idx: usize = 0;
+    while (blk_idx < enc.len) : (blk_idx += blocksize) {
+        var blk = enc[blk_idx .. blk_idx + blocksize];
+        if (seen_blocks.contains(blk)) return true;
+        _ = try seen_blocks.put(blk, {});
+    }
+
+    return false;
+}
+
 pub fn discoverSecretSuffix(allocator: *mem.Allocator, oracle: EncryptionOracle(128)) !void {
     const blocksize = try discoverBlockSize(allocator, oracle);
     std.debug.warn("\nfound the blocksize: {}", .{blocksize});
+    const isECB = try isOracleECB(allocator, blocksize, oracle);
+    std.debug.warn("\nis it ECB? {}\n", .{isECB});
+
     var enc = try oracle.encrypt("hello world");
     allocator.free(enc);
 }
